44da44f51bcd8b499bb4c9d06640bc49d1a8bcad
James Rickelton <jjarickelton@gmail.com>
2021-03-19 12:16:53 +0000

    Added start script for heroku

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,15 @@
 const dbConnection = require("knex");
-const dbConfig = require("../knexfile");
+const dbConfig =
+  ENV === "production"
+    ? {
+        client: "pg",
+        connection: {
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : require("../knexfile");
 
 exports.dbConnection = dbConnection(dbConfig);
